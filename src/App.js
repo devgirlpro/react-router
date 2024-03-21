bb0ab1ba1304f4de3d20af10048a703534a7fb88
devgirlpro <agalidari@gmail.com>
2024-03-21 20:08:20 +0100

    add structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-
 import './App.css';
+import Layout from './layout/Layout';
 
 function App() {
   return (
-    <div className="App">
+    <Layout>
       <h1>React Router</h1>
-    </div>
+    </Layout>
   );
 }
 
