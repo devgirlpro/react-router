4c46b264cc24491201d5bec7ea89fc0a79d51129
devgirlpro <agalidari@gmail.com>
2024-03-21 20:18:48 +0100

    install react-router-dom and add some pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import Layout from './layout/Layout';
+import Home from './pages/Home';
+import Users from './pages/Users';
+import Contact from './pages/Contact';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Layout>
-      <h1>React Router</h1>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/users' element={<Users />} />
+        <Route path='/contact' element={<Contact /> } />
+      </Routes>
+      </BrowserRouter>
     </Layout>
-  );
+  )
 }
 
 export default App;
